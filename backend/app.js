d582b48db449f10f24c3f079bdfde463d53fab04
Priya Yasasvin <priyayasasvin@priyas-air.home>
2022-02-20 19:53:09 +0000

    first commit

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { response } = require("express");
+const path = require('path');
+const cors = require('cors')
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+module.exports=app;
